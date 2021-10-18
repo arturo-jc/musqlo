928e876fd5fffc573fd54597e75546fd2eb5ee51
Arturo <arturo.javcast@yahoo.com.mx>
2021-10-17 21:58:10 -0500

    uncomment db.create_all()

--- main.py
+++ main.py
@@ -151,15 +151,15 @@ class Set(db.Model):
     exercise_id = Column(Integer, ForeignKey("exercises.id"))
     parent_exercise = relationship("Exercise", back_populates="sets")
 
-# # CREATE DB THEN COMMENT OUT
-# db.create_all()
-#
-# # POPULATE DAYS TABLE THEN COMMENT OUT
-# days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-# for day in days:
-#     new_day = Day(name=day)
-#     db.session.add(new_day)
-# db.session.commit()
+# CREATE DB THEN COMMENT OUT
+db.create_all()
+
+# POPULATE DAYS TABLE THEN COMMENT OUT
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+for day in days:
+    new_day = Day(name=day)
+    db.session.add(new_day)
+db.session.commit()
 
 
 # USER AUTHENTICATION
