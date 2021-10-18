7b8ceaf7cd690cf63e4edb77eaf961674ae0840b
Arturo <arturo.javcast@yahoo.com.mx>
2021-10-17 20:48:42 -0500

    change sqlite to posgres

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 Bootstrap(app)
 
 # CONNECT TO DATABASE
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///workout.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///workout.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
